5326ec55b238e5e68cc1ca6d7bd27b95804d8eba
cjwsstrm <cjwesstrom@gmail.com>
2017-09-19 23:38:02 +0000

    added charactercount functionality to .counter class

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,20 @@
-$(document).ready(function() {
-  console.log("READY!");
-
-  
+$(function() {
+  $('.new-tweet').on('input', 'textarea', function () {
+    const counter = $(this).closest('form').find('.counter');
+    const inputLength = $(this).val().length;
+    const remainingBudget = 140 - inputLength;
+    counter.text(remainingBudget);
+    if (remainingBudget < 0) {
+      $('.counter').css('color', 'red');
+    }
+    else $('.counter').css('color', '#244751');
+  });
 });
+
+
+// if (this.value.length !== 0) {
+// }
+
+// if (charCount < 0) {
+// }
+// console.log(140 - inputLength);
