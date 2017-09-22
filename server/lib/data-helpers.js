029207998ee28587df3710fdf8646b030f7e5c71
cjwsstrm <cjwesstrom@gmail.com>
2017-09-22 00:46:02 +0000

    linted comments

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,26 +1,17 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
-// const simulateDelay = require("./util/simulate-delay");
-
-// Defines helper functions for saving and getting tweets, using the database `db`
+// Defines helper functions for saving and getting tweets
 module.exports = function makeDataHelpers(db) {
   return {
 
-    // Saves a tweet to `db`
+    // Saves a tweet to db
     saveTweet: function(newTweet, callback) {
-        db.collection("tweets").insertOne(newTweet);
-        callback(null, true);
+        db.collection("tweets").insertOne(newTweet, callback);
     },
 
-    // Get all tweets in `db`, sorted by newest first
+    // Get all tweets in db
     getTweets: function(callback) {
-      db.collection("tweets").find().toArray((err, tweets) => {
-        if (err) {
-          return callback(err);
-        }
-        callback(null, tweets);
-      });
+      db.collection("tweets").find().toArray((callback);
     }
   };
 }
